3031311dd668548a6318725c86430f75c0fab8f5
Anth0nyPereira <60923555+Anth0nyPereira@users.noreply.github.com>
2020-06-07 19:33:16 +0100

    Primeira conexão à BD yay

---
+++ Pets_At_First_Sight/Pets_At_First_Sight/Classes/SQLServerConnection.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Windows;
+
+namespace Pets_At_First_Sight.Classes
+{
+    class SQLServerConnection
+    {
+
+
+        public static String GetConnectionStrings()
+        {
+            String connectionString = ConfigurationManager.ConnectionStrings["connectionStrings"].ToString();
+            return connectionString;
+        }
+
+        public static String sql;
+        public static SqlConnection connection = new SqlConnection();
+        public static SqlCommand command = new SqlCommand("", connection);
+        public static SqlDataReader reader;
+        public static DataTable datatable;
+        public static SqlDataAdapter adapter;
+
+        public static void openConnection()
+        {
+            try
+            {
+                if (connection.State == ConnectionState.Closed)
+                {
+                    connection.ConnectionString = GetConnectionStrings();
+                    connection.Open();
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Falhou a conexão", "Tente Novamente!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public static void closeConnection()
+        {
+            try
+            {
+                if (connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Falhou a conexão", "Tente Novamente!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+
+}
---
+++ Pets_At_First_Sight/Pets_At_First_Sight/Teste.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Data;
+using System.Data.SqlClient;
+using Pets_At_First_Sight.Classes;
+
+namespace Pets_At_First_Sight
+{
+    /// <summary>
+    /// Interaction logic for Teste.xaml
+    /// </summary>
+    public partial class Teste : Page
+    {
+        public Teste()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            SQLServerConnection.openConnection();
+            SQLServerConnection.sql = "SELECT [nome] FROM projeto.ANIMAL";
+            SQLServerConnection.command.CommandType = CommandType.Text;
+            SQLServerConnection.command.CommandText = SQLServerConnection.sql;
+            SQLServerConnection.adapter = new SqlDataAdapter(SQLServerConnection.command);
+            SQLServerConnection.datatable = new DataTable();
+            SQLServerConnection.adapter.Fill(SQLServerConnection.datatable);
+            datagrid.ItemsSource = SQLServerConnection.datatable.DefaultView;
+            SQLServerConnection.closeConnection();
+        }
+    }
+}
